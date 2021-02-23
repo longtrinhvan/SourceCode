4d5ba54dacf5f8c321a0e8d2f5aa8b3fbfec1467
Long Trinh <longtrinhvan97@gmail.com>
2021-02-23 12:22:31 +0700

    setup project

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.impl.InstructorDao;
+import com.example.demo.entity.Instructor;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	public InstructorDao instructorDao;
+
+	@RequestMapping("/home")
+	public String listCustomer() {
+		List<Instructor> a = instructorDao.findAll();
+		for (Instructor x : a) {
+			System.out.println(x.getEmail());
+		}
+		return "customer-list";
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/dao/impl/InstructorDao.java
@@ -0,0 +1,27 @@
+package com.example.demo.dao.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.dao.IInstructorDAO;
+import com.example.demo.entity.Instructor;
+
+@Transactional
+@Repository
+public class InstructorDao implements IInstructorDAO{
+	
+	@Autowired
+	protected SessionFactory sessionFactory;
+	
+
+	public List<Instructor> findAll() {
+		Session session = this.sessionFactory.getCurrentSession();
+	    return session.createQuery("FROM Instructor", Instructor.class).getResultList();
+	}
+}
