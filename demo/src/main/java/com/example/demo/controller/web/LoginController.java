bd64966c4a6e15dd07aba23eb68be9da3a1c78d8
Long Trinh <longtrinhvan97@gmail.com>
2021-02-25 05:53:21 +0700

    setup product

--- demo/src/main/java/com/example/demo/controller/web/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/web/LoginController.java
@@ -1,22 +1,10 @@
 package com.example.demo.controller.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-	@GetMapping("/loginAdmin")
-	public String login(Model model) {
-		
-		return "login/login";
-	}
 	
-	@GetMapping("/loginAdmin1")
-	public String login1(Model model) {
-		
-		return "login/login/login";
-	}
 	
 }
