72008b929e381e41509bf7bf2b56a01b8745350f
Long Trinh <longtrinhvan97@gmail.com>
2021-02-25 03:26:23 +0700

    setup project

--- demo/src/main/java/com/example/demo/controller/web/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/web/LoginController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
+	@GetMapping("/loginAdmin")
+	public String login(Model model) {
+		
+		return "non-layout/login";
+	}
 	
 }
